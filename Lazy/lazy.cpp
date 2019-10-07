9a237035539a6ff464e777a6344891c85dbfc6d2
张峰 <yonghenghuanmie@hotmail.com>
2019-10-07 08:07:40 +0800

    add log

--- Lazy/lazy.cpp
+++ Lazy/lazy.cpp
@@ -1,9 +1,9 @@
 #include <cmath>
-#include <tuple>
 #include <typeinfo>
 #include <iostream>
 #include <functional>
 #include <type_traits>
+#include <Eigen/Dense>
 
 namespace lazy
 {
@@ -24,150 +24,162 @@ namespace lazy
 	};
 
 	template<typename T>
-	concept LazyRequired = requires(T t)
+	concept LazyRequired = requires(T v)
 	{
 		std::is_arithmetic_v<T> || std::is_invocable_v<T>;
 	};
 
 	// +
-	template<LazyRequired T1, LazyRequired T2, bool IsArithmetic1 = std::is_arithmetic_v<T1>, bool IsArithmetic2 = std::is_arithmetic_v<T2>>
+	template<LazyRequired T1, LazyRequired T2, bool IsInvocable1 = std::is_invocable_v<T1>, bool IsInvocable2 = std::is_invocable_v<T2>>
 	auto operator+(const Value<T1>& value1, const T2& value2)
 	{
-		if constexpr (IsArithmetic1)
+		if constexpr (IsInvocable1)
 		{
-			if constexpr (IsArithmetic2)
+			if constexpr (IsInvocable2)
 			{
-				return Value([&]() {return static_cast<T1>(value1) + value2; });
+				return Value([&] {return value1() + value2(); });
 			}
 			else
 			{
-				return Value([&]() {return static_cast<T1>(value1) + value2(); });
+				return Value([&] {return value1() + static_cast<T2>(value2); });
 			}
 		}
 		else
 		{
-			if constexpr (IsArithmetic2)
+			if constexpr (IsInvocable2)
 			{
-				return Value([&]() {return value1() + static_cast<T2>(value2); });
+				return Value([&] {return static_cast<T1>(value1) + value2(); });
 			}
 			else
 			{
-				return Value([&]() {return value1() + value2(); });
+				return Value([&] {return static_cast<T1>(value1) + value2; });
 			}
 		}
 	}
 
 	// -
-	template<LazyRequired T1, LazyRequired T2, bool IsArithmetic1 = std::is_arithmetic_v<T1>, bool IsArithmetic2 = std::is_arithmetic_v<T2>>
+	template<LazyRequired T1, LazyRequired T2, bool IsInvocable1 = std::is_invocable_v<T1>, bool IsInvocable2 = std::is_invocable_v<T2>>
 	auto operator-(const Value<T1>& value1, const T2& value2)
 	{
-		if constexpr (IsArithmetic1)
+		if constexpr (IsInvocable1)
 		{
-			if constexpr (IsArithmetic2)
+			if constexpr (IsInvocable2)
 			{
-				return Value([&]() {return static_cast<T1>(value1) - value2; });
+				return Value([&] {return value1() - value2(); });
 			}
 			else
 			{
-				return Value([&]() {return static_cast<T1>(value1) - value2(); });
+				return Value([&] {return value1() - static_cast<T2>(value2); });
 			}
 		}
 		else
 		{
-			if constexpr (IsArithmetic2)
+			if constexpr (IsInvocable2)
 			{
-				return Value([&]() {return value1() - static_cast<T2>(value2); });
+				return Value([&] {return static_cast<T1>(value1) - value2(); });
 			}
 			else
 			{
-				return Value([&]() {return value1() - value2(); });
+				return Value([&] {return static_cast<T1>(value1) - value2; });
 			}
 		}
 	}
 
 	// *
-	template<LazyRequired T1, LazyRequired T2, bool IsArithmetic1 = std::is_arithmetic_v<T1>, bool IsArithmetic2 = std::is_arithmetic_v<T2>>
+	template<LazyRequired T1, LazyRequired T2, bool IsInvocable1 = std::is_invocable_v<T1>, bool IsInvocable2 = std::is_invocable_v<T2>>
 	auto operator*(const Value<T1>& value1, const T2& value2)
 	{
-		if constexpr (IsArithmetic1)
+		if constexpr (IsInvocable1)
 		{
-			if constexpr (IsArithmetic2)
+			if constexpr (IsInvocable2)
 			{
-				return Value([&]() {return static_cast<T1>(value1)* value2; });
+				return Value([&] {return value1() * value2(); });
 			}
 			else
 			{
-				return Value([&]() {return value1 == 0 ? 0 : static_cast<T1>(value1)* value2(); });
+				return Value([&] {return value2 == 0 ? 0 : value1() * static_cast<T2>(value2); });
 			}
 		}
 		else
 		{
-			if constexpr (IsArithmetic2)
+			if constexpr (IsInvocable2)
 			{
-				return Value([&]() {return value2 == 0 ? 0 : value1() * static_cast<T2>(value2); });
+				return Value([&] {return value1 == 0 ? 0 : static_cast<T1>(value1)* value2(); });
 			}
 			else
 			{
-				return Value([&]() {return value1() * value2(); });
+				return Value([&] {return static_cast<T1>(value1)* value2; });
 			}
 		}
 	}
 
 	// /
-	template<LazyRequired T1, LazyRequired T2, bool IsArithmetic1 = std::is_arithmetic_v<T1>, bool IsArithmetic2 = std::is_arithmetic_v<T2>>
+	template<LazyRequired T1, LazyRequired T2, bool IsInvocable1 = std::is_invocable_v<T1>, bool IsInvocable2 = std::is_invocable_v<T2>>
 	auto operator/(const Value<T1>& value1, const T2& value2)
 	{
-		if constexpr (IsArithmetic1)
+		if constexpr (IsInvocable1)
 		{
-			if constexpr (IsArithmetic2)
+			if constexpr (IsInvocable2)
 			{
-				return Value([&]() {return static_cast<T1>(value1) / value2; });
+				return Value([&] {return value1() / value2(); });
 			}
 			else
 			{
-				return Value([&]() {return value1 == 0 ? 0 : static_cast<T1>(value1) / value2(); });
+				return Value([&] {return value1() / static_cast<T2>(value2); });
 			}
 		}
 		else
 		{
-			if constexpr (IsArithmetic2)
+			if constexpr (IsInvocable2)
 			{
-				return Value([&]() {return value1() / static_cast<T2>(value2); });
+				return Value([&] {return value1 == 0 ? 0 : static_cast<T1>(value1) / value2(); });
 			}
 			else
 			{
-				return Value([&]() {return value1() / value2(); });
+				return Value([&] {return static_cast<T1>(value1) / value2; });
 			}
 		}
 	}
 
-	template<LazyRequired T1, LazyRequired T2, bool IsArithmetic1 = std::is_arithmetic_v<T1>, bool IsArithmetic2 = std::is_arithmetic_v<T2>>
+	template<LazyRequired T1, LazyRequired T2, bool IsInvocable1 = std::is_invocable_v<T1>, bool IsInvocable2 = std::is_invocable_v<T2>>
 	auto pow(const T1& base, const T2& index)
 	{
-		if constexpr (IsArithmetic1)
+		if constexpr (IsInvocable1)
 		{
-			if constexpr (IsArithmetic2)
+			if constexpr (IsInvocable2)
 			{
-				return Value([&]() {return std::pow(base, index); });
+				return Value([&] {return std::pow(base(), index()); });
 			}
 			else
 			{
-				return Value([&]() {return base == 1 ? 1 : std::pow(base, index());; });
+				return Value([&] {return index == 0 ? 1 : std::pow(base(), index); });
 			}
 		}
 		else
 		{
-			if constexpr (IsArithmetic2)
+			if constexpr (IsInvocable2)
 			{
-				return Value([&]() {return index == 0 ? 1 : std::pow(base(), index); });
+				return Value([&] {return base == 1 ? 1 : std::pow(base, index());; });
 			}
 			else
 			{
-				return Value([&]() {return std::pow(base(), index()); });
+				return Value([&] {return std::pow(base, index); });
 			}
 		}
 	}
 
+	template<LazyRequired T, bool IsArithmetic = std::is_arithmetic_v<T>>
+	auto log(const T& value)
+	{
+		if constexpr (IsArithmetic)
+		{
+			return value == 1 ? 0 : [&] {return std::log(value); };
+		}
+		else
+		{
+			return [&] {return std::log(value()); };
+		}
+	}
 }
 
 int main()
@@ -177,7 +189,7 @@ int main()
 	auto y10 = (lazy::Value([]() {return 10; }) * 10)();
 	auto y11 = (lazy::Value([]() {return 10; }) * []() {return 10; })();*/
 	auto func_y1 = lazy::pow(1, lazy::pow(10, 2) * 5) * 10;
-	auto func_y2 = (lazy::Value(1.0) + 10) * 20 / 2 - []() {return 4 + 8; }-lazy::pow(4, 2);
+	auto func_y2 = (lazy::Value(1.0) + 10) * 20 / lazy::log(2) - []() {return 4 + 8; }-lazy::pow(4, 2);
 	auto y1 = func_y1();
 	auto y2 = func_y2();
 	return 0;
