8dd7cea5464591a65ded4d5e3866a5c0ce445ae4
huanmie <yonghenghuanmie@gmail.com>
2022-04-27 04:17:50 +0800

    Add: Support position in template parameters

--- ConstraintType/ConstraintType.cpp
+++ ConstraintType/ConstraintType.cpp
@@ -3,8 +3,11 @@
 *	@date 2022.4.26
 */
 
+
+// Don't forget <tuple> and <concept>
 #include <set>
 #include <list>
+#include <tuple>
 #include <deque>
 #include <vector>
 #include <concepts>
@@ -17,28 +20,51 @@ struct Short { short value; };
 struct Integer { int value; };
 struct Long { long long value; };
 
+template<typename T1, typename T2>
+struct A {};
+
+template<typename T1, typename T2>
+struct B {};
+
 namespace ConstraintType
 {
 	ConstructBasicEligibleType(BasicEligibleType, Byte, Short, Integer, Long);
 
-	//[1]
+	//Layer:[1]
+	//std::vector<Byte>
 	AddTypeLayer(1, std::list);
 	AddTypeLayer(1, std::deque);
 	AddTypeLayer(1, std::vector);
 	AddTypeLayer(1, std::forward_list);
 	Construct1LayerEligibleType(EligibleType1, 1, Byte, Short, Integer, Long);
 
-	//[2,1]
+	//Layer:[2,1]. Use common layer.
 	AddTypeLayer(2, std::set);
+	//std::set<std::vector<Integer>>
 	Construct2LayerEligibleType(EligibleType2, 2, Byte, Short, Integer, Long);
 
-	//Use different layer to avoid collision.[11,10]
+	//Layer:[11,10]. Use different layer to avoid collision.
 	AddTypeLayer(10, std::vector);
 	AddTypeLayer(11, std::vector);
+	//std::vector<std::vector<Short>>
 	Construct2LayerEligibleType(EligibleType3, 11, Byte, Short, Integer, Long);
+
+	AddTypeLayerWithPosition(0x10, 1, A);
+	//A<?,Byte>
+	Construct1LayerEligibleTypeWithPosition(EligibleType4, 0x10, 1, Byte);
+
+	//Same with AddTypeLayer(0x11, B); if B<T2> has default template parameter.
+	AddTypeLayerWithPosition(0x11, 0, B);
+	//B<A<?,Long>,?>
+	Construct2LayerEligibleTypeWithPosition(EligibleType5, 0x11, 0, 1, Long);
+
+	AddTypeLayerWithPosition(0x11, 1, B);
+	//B<?,int>
+	// You can combine EligibleType5 and EligibleType6 to construct more complicated constraints.
+	Construct1LayerEligibleTypeWithPosition(EligibleType6, 0x11, 1, int);
 }
 
-class A
+class Test
 {
 public:
 	template<ConstraintType::BasicEligibleType T>
@@ -58,22 +84,48 @@ public:
 
 	template<ConstraintType::EligibleType3 T>
 	static void TestEligibleType3(const T& c) {}
+
+	template<ConstraintType::EligibleType4 T>
+	static void TestEligibleType4(const T& c) {}
+
+	template<ConstraintType::EligibleType5 T>
+	static void TestEligibleType5(const T& c) {}
+
+	//B<A<?,Long>,int>
+	template<typename T> requires ConstraintType::EligibleType5<T>&& ConstraintType::EligibleType6<T>
+	static void TestCombinedEligibleType(const T& c) {}
 };
 
 int main()
 {
 	//A::TestBasicEligibleType(1);										//Wrong: int not in basic eligible type set.
-	A::TestBasicEligibleType(Long{ 1 });								//OK
+	Test::TestBasicEligibleType(Long{ 1 });								//OK
 
 	//A::TestEligibleType1(std::vector<int>());							//Wrong: int not in basic eligible type set.
 	//A::TestEligibleType1(std::set<Byte>());							//Wrong: std::set not in layer1's eligible type set
-	A::TestEligibleType1(std::vector<Byte>{ { 1 }, { 2 }, { 3 } });		//OK
+	Test::TestEligibleType1(std::vector<Byte>{ { 1 }, { 2 }, { 3 } });	//OK
 
 	//A::TestEligibleType2(std::vector<std::vector<Integer>>());		//Wrong: std::vector not in layer2's eligible type set
 	//A::TestEligibleType2(std::set<std::set<Integer>>());				//Wrong: std::set not in layer1's eligible type set
 	//A::TestEligibleType2(std::set<std::vector<int>>());				//Wrong: int not in basic eligible type set.
-	A::TestEligibleType2(std::set<std::vector<Integer>>());				//OK
+	Test::TestEligibleType2(std::set<std::vector<Integer>>());			//OK
 
-	A::TestEligibleType3(std::vector<std::vector<Short>>());			//OK
+	Test::TestEligibleType3(std::vector<std::vector<Short>>());			//OK
+
+	//Test::TestEligibleType4(A<int, Long>());							//Wrong: second template parameter not in eligible type set
+	Test::TestEligibleType4(A<int, Byte>());
+
+	//Test::TestEligibleType5(B<A<int, int>, int>());					//Wrong: int not in basic eligible type set.
+	//Test::TestEligibleType5(B<B<int,Long>,int>());					//Wrong: B not in layer1's eligible type set
+	//Test::TestEligibleType5(A<A<int, Long>, int>());					//Wrong: A not in layer2's eligible type set
+	//B<A<?,Long>,?>
+	Test::TestEligibleType5(B<A<int, Long>, int>());					//OK
+	Test::TestEligibleType5(B<A<double, Long>, int>());					//OK
+	Test::TestEligibleType5(B<A<int, Long>, char>());					//OK
+
+	//B<A<?,Long>,int>
+	//Test::TestCombinedEligibleType(B<A<int, Long>, double>());		//Wrong: double doesn't meet EligibleType6's requirements(int)
+	//Test::TestCombinedEligibleType(B<A<int, double>, int>());			//Wrong: double doesn't meet EligibleType5's requirements(Long)
+	Test::TestCombinedEligibleType(B<A<int, Long>, int>());				//OK
 	return 0;
 }
--- ConstraintType/ConstraintType.h
+++ ConstraintType/ConstraintType.h
@@ -11,33 +11,62 @@ namespace ConstraintType
 	concept _EligibleUnderlyingType = (std::same_as<T, Rest> || ...);
 
 	using Layer = std::size_t;
-	template<typename T, Layer layer>
+	using Index = std::size_t;
+	template<typename T, Layer layer, Index index>
 	struct _IsEligibleType :std::false_type {};
 
-#define _GetUnderlyingType(LayerNumber,CurrentType)													\
-	std::enable_if_t<_IsEligibleType<CurrentType,LayerNumber>::value, typename _IsEligibleType<CurrentType,LayerNumber>::underlying_type>
+	constexpr std::size_t default_index = 0;
+#define _GetUnderlyingType(LayerNumber,CurrentType)														\
+	std::enable_if_t<_IsEligibleType<CurrentType,LayerNumber,default_index>::value,						\
+	typename _IsEligibleType<CurrentType,LayerNumber,default_index>::underlying_type>
 
-#define AddTypeLayer(LayerNumber,Type)																\
-	template<typename T>																			\
-	struct _IsEligibleType<Type<T>,LayerNumber> :std::true_type										\
-	{																								\
-		using underlying_type = T;																	\
+#define _GetUnderlyingTypeWithPosition(LayerNumber,CurrentType,TypeIndex)								\
+	std::enable_if_t<_IsEligibleType<CurrentType,LayerNumber,TypeIndex>::value,							\
+	typename _IsEligibleType<CurrentType,LayerNumber,TypeIndex>::underlying_type>
+
+#define AddTypeLayer(LayerNumber,Type)																	\
+	template<typename T>																				\
+	struct _IsEligibleType<Type<T>,LayerNumber,default_index> :std::true_type							\
+	{																									\
+		using underlying_type = T;																		\
 	};
 
-#define ConstructBasicEligibleType(Name,...)														\
-	template<typename T>																			\
+#define ConstructBasicEligibleType(Name,...)															\
+	template<typename T>																				\
 	concept Name = _EligibleUnderlyingType<T,##__VA_ARGS__>;
 
 	//Please make sure StartLayer - layer number >=0
-#define Construct1LayerEligibleType(Name,StartLayer,...)											\
-	template<typename T>																			\
+#define Construct1LayerEligibleType(Name,StartLayer,...)												\
+	template<typename T>																				\
 	concept Name = _EligibleUnderlyingType<_GetUnderlyingType(StartLayer,T),##__VA_ARGS__>;
 
-#define Construct2LayerEligibleType(Name,StartLayer,...)											\
-	template<typename T>																			\
+#define Construct2LayerEligibleType(Name,StartLayer,...)												\
+	template<typename T>																				\
 	concept Name = _EligibleUnderlyingType<_GetUnderlyingType(StartLayer-1,_GetUnderlyingType(StartLayer,T)),##__VA_ARGS__>;
 
-#define Construct3LayerEligibleType(Name,StartLayer,...)											\
-	template<typename T>																			\
+#define Construct3LayerEligibleType(Name,StartLayer,...)												\
+	template<typename T>																				\
 	concept Name = _EligibleUnderlyingType<_GetUnderlyingType(StartLayer-2,_GetUnderlyingType(StartLayer-1,_GetUnderlyingType(StartLayer,T))),##__VA_ARGS__>;
+
+
+#define AddTypeLayerWithPosition(LayerNumber,TypeIndex,Type)											\
+	template<typename... Rest>																			\
+	struct _IsEligibleType<Type<Rest...>,LayerNumber,TypeIndex> :std::true_type							\
+	{																									\
+		using underlying_type = std::tuple_element_t<TypeIndex,std::tuple<Rest...>>;					\
+	};
+
+#define Construct1LayerEligibleTypeWithPosition(Name,StartLayer,TypeIndex1,...)							\
+	template<typename T>																				\
+	concept Name = _EligibleUnderlyingType<_GetUnderlyingTypeWithPosition(StartLayer,T,TypeIndex1),##__VA_ARGS__>;
+
+#define Construct2LayerEligibleTypeWithPosition(Name,StartLayer,TypeIndex1,TypeIndex2,...)				\
+	template<typename T>																				\
+	concept Name = _EligibleUnderlyingType<_GetUnderlyingTypeWithPosition(StartLayer-1,		\
+	_GetUnderlyingTypeWithPosition(StartLayer,T,TypeIndex1),TypeIndex2),##__VA_ARGS__>;
+
+#define Construct3LayerEligibleTypeWithPosition(Name,StartLayer,TypeIndex1,TypeIndex2,TypeIndex3,...)	\
+	template<typename T>																				\
+	concept Name = _EligibleUnderlyingType<_GetUnderlyingTypeWithPosition(StartLayer-2,		\
+	_GetUnderlyingTypeWithPosition(StartLayer-1,_GetUnderlyingTypeWithPosition(StartLayer,T,TypeIndex1),TypeIndex2),TypeIndex3),##__VA_ARGS__>;
 }
