513c332e0b231768b2f6dc6bf7c829f287cf333a
huanmie <yonghenghuanmie@gmail.com>
2022-07-20 05:48:37 +0800

    Modified: use Graph to simplify

--- StateMachine/StateMachine.cpp
+++ StateMachine/StateMachine.cpp
@@ -1,11 +1,16 @@
 #include <cstdlib>
 #include <memory>
 #include <string>
+#include <numeric>
 #include <optional>
 #include <iostream>
+#include <execution>
+#include <algorithm>
 #include <functional>
 #include <string_view>
+#include <unordered_set>
 #include <unordered_map>
+#include "Graph.h"
 #include "StateMachine.h"
 
 enum class Thread
@@ -16,30 +21,25 @@ enum class Thread
 class A :public StateMachine<Thread, std::string, A>
 {
 public:
-	A()
+	A() :StateMachine(Thread::running, "",
+		Thread::running, "to_block", Thread::block,
+		Thread::block, "to_running", Thread::running,
+		Thread::running, "to_destroy", Thread::destroy,
+		Thread::block, "to_destroy", Thread::destroy
+	)
 	{
-		Register_Callable(Thread::running, [](A*) {std::cout << "running" << std::endl; return true; },
-			Thread::block, [](A*) {std::cout << "block" << std::endl; return true; },
-			Thread::destroy, [](A*) {std::cout << "destroy" << std::endl; return true; });
-
-		Register_Relation(Thread::running, "to_block", Thread::block,
-			Thread::block, "to_running", Thread::running,
-			Thread::running, "to_destroy", Thread::destroy,
-			Thread::block, "to_destroy", Thread::destroy);
-
-		InitialState(Thread::running);
+		auto print_state = [](A*, Thread old_state, Thread new_state) {std::cout << "transform from " << (int)old_state << " to " << (int)new_state << std::endl; return true; };
+		RegisterCallback(Thread::running, print_state, Thread::block, print_state, Thread::destroy, print_state);
 	}
-private:
 };
 
 int main()
 {
 	A a;
-	std::cout << a.PerformOperation("to_block") << std::endl;
-	std::cout << a.PerformOperation("to_block") << std::endl;
-	std::cout << a.RepeatOperation() << std::endl;
-	std::cout << a.PerformOperation("to_destroy") << std::endl;
-	std::cout << a.PerformOperation("to_running") << std::endl;
-	std::system("pause");
+	std::cout << std::boolalpha
+		<< a.PerformOperation("to_block") << "\n"				// true
+		<< a.PerformOperation("to_block") << "\n"				// false
+		<< a.PerformOperation("to_destroy") << "\n"				// true
+		<< a.PerformOperation("to_running") << std::endl;		// false
 	return 0;
 }
--- StateMachine/StateMachine.h
+++ StateMachine/StateMachine.h
@@ -1,105 +1,55 @@
 #pragma once
 
 template<class StateType, class Operation, class Derive>
-class StateMachine
+class StateMachine :public Graph<StateType, Operation>
 {
 public:
-	auto GetState() noexcept
+	StateType GetState() noexcept
 	{
 		return state;
 	}
 
-	auto GetNextState() noexcept
-	{
-		return next_state;
-	}
-
 	bool PerformOperation(const Operation& operation)
 	{
-		if ((bool)state == false)
-			return false;
-		auto operation_list = relation.find(state.value());
-		if (operation_list == relation.end())
-			return false;
-		auto pair = operation_list->second.find(operation);
-		if (pair == operation_list->second.end())
+		if (derive == nullptr && (derive = dynamic_cast<Derive*>(this)) == nullptr)
 			return false;
 
-		Derive* derive = dynamic_cast<Derive*>(this);
-		if (derive == nullptr)
-			return false;
-		next_state = pair->second;
-		if (callable_map[*next_state](derive))
-			state = next_state;
-		next_state = std::nullopt;
-		return true;
-	}
-
-	bool RepeatOperation()
-	{
-		if ((bool)state == false)
-			return false;
-		auto iterator = callable_map.find(state.value());
-		if (iterator == callable_map.end())
-			return false;
-		Derive* derive = dynamic_cast<Derive*>(this);
-		if (derive == nullptr)
-			return false;
-
-		next_state = state;
-		callable_map[*next_state](derive);
-		next_state = std::nullopt;
-		return true;
+		auto paths = this->GetOutPath(state);
+		for (auto&& [new_state, op] : paths)
+			if (op == operation)
+			{
+				if (callback_map[new_state](derive, state, new_state))
+				{
+					state = new_state;
+					return true;
+				}
+				else
+					return false;
+			}
+		return false;
 	}
 
 protected:
-	using callable = std::function<bool(Derive*)>;
+	using Callable = std::function<bool(Derive*, StateType, StateType)>;
 
-	StateMachine() = default;
+	StateMachine(const StateType& initial_state, const Operation& invalid, auto&&... rests)
+		:Graph<StateType, Operation>(invalid, std::forward<decltype(rests)>(rests)...), state(initial_state) {}
+
 	virtual ~StateMachine() {}
 
-	template<class... Rest>
-	void Register_Callable(const StateType& s, callable&& function, Rest&&... rest)
+	void RegisterCallback(const StateType& state, Callable&& function, auto&&... rests)
 	{
-		callable_map[s] = std::move(function);
-		Register_Callable(rest...);
-	}
-
-	template<class... Rest>
-	bool Register_Relation(const StateType& begin, const Operation& operation, const StateType& end, Rest&&... rest)
-	{
-		auto iterator = callable_map.find(begin);
-		if (iterator == callable_map.end())
-			return false;
-		iterator = callable_map.find(end);
-		if (iterator == callable_map.end())
-			return false;
-
-		if (Register_Relation(rest...) == false)
-			return false;
-		relation[begin].emplace(operation, end);
-		return true;
-	}
-
-	bool InitialState(const StateType& s)
-	{
-		auto pair = callable_map.find(s);
-		if (pair == callable_map.end())
-			return false;
-		Derive* derive = dynamic_cast<Derive*>(this);
-		if (derive == nullptr)
-			return false;
-		state = s;
-		callable_map[s](derive);
-		return true;
+		callback_map[state] = std::move(function);
+		RegisterCallback(rests...);
 	}
 
 private:
-	void Register_Callable() {}
-	bool Register_Relation() { return true; }
+	void RegisterCallback(const StateType& state, Callable&& function)
+	{
+		callback_map[state] = std::move(function);
+	}
 
-	std::optional<StateType> state;
-	std::optional<StateType> next_state;
-	std::unordered_map<StateType, callable> callable_map;
-	std::unordered_map<StateType, std::unordered_map<Operation, StateType>> relation;
+	StateType state;
+	Derive* derive = nullptr;
+	std::unordered_map<StateType, Callable> callback_map;
 };
